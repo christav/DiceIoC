1057fa96fb38bd4aaa676a19efe7ae83f53661d3
Chris Tavares <ctavares@microsoft.com>
2013-08-10 00:12:38 -0700

    Added unit test project

---
+++ DiceIoC.Tests/Basics/SimpleRegistrations.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using DiceIoC.Tests.SampleTypes;
+using FluentAssertions;
+using Xunit;
+
+namespace DiceIoC.Tests.Basics
+{
+    public class SimpleRegistrations : IDisposable
+    {
+        private Container container;
+
+        public SimpleRegistrations()
+        {
+            container = new Container();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void DefaultContainerHasNoRegistrations()
+        {
+            container.Registrations.Count().Should().Be(0);
+        }
+
+        [Fact]
+        public void ResolvingUnregisteredTypeShouldThrow()
+        {
+            Assert.Throws<ArgumentException>(() => container.Resolve<SimpleRegistrations>());
+        }
+
+        [Fact]
+        public void ResolvingRegisteredTypeSucceeds()
+        {
+            container.Register(c => new ConcreteClass());
+        }
+
+        [Fact]
+        public void ResolvingInvokesRegisteredDelegate()
+        {
+            bool called = false;
+            container.Register(c =>
+                {
+                    called = true;
+                    return new ConcreteClass();
+                });
+
+            container.Resolve<ConcreteClass>();
+            called.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ContainerPassedToDelegateIsResolvingContainer()
+        {
+            Container passedContainer = null;
+            container.Register(c =>
+                {
+                    passedContainer = c;
+                    return new ConcreteClass();
+                });
+
+            container.Resolve<ConcreteClass>();
+            passedContainer.Should().BeSameAs(container);
+        }
+
+        [Fact]
+        public void CanRegisterInterface()
+        {
+            container.Register<ISimpleInterface>(c => new SimpleInterfaceImpl());
+
+            container.Resolve<ISimpleInterface>().Should().BeOfType<SimpleInterfaceImpl>();
+        }
+
+        [Fact]
+        public void FactMethodName()
+        {
+            
+        }
+    }
+}
---
+++ DiceIoC.Tests/Bootstrap/CanRun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace DiceIoC.Tests.Bootstrap
+{
+    public class CanRun
+    {
+        [Fact]
+        public void CanRunThisTest()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
