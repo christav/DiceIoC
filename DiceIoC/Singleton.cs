b53e8c750e437303249cf02c34b928cbddfbf868
Chris Tavares <ctavares@microsoft.com>
2013-11-15 15:07:01 -0800

    Futzing with singleton implementation.

--- DiceIoC/Singleton.cs
+++ DiceIoC/Singleton.cs
@@ -5,16 +5,22 @@ namespace DiceIoC
     public class Singleton
     {
         private object value;
-
+        private Func<Container, string, Type, object> factory;
+ 
         private Func<Container, string, Type, object> LifetimeModifier(Func<Container, string, Type, object> factory)
         {
-            return (c, n, t) => value ?? (value = factory(c, n, t));
+            this.factory = factory;
+            return this.GetValue;
+        }
+
+        private object GetValue(Container c, string name, Type t)
+        {
+            return value ?? (value = factory(c, name, t));
         }
 
         public static Func<Func<Container, string, Type, object>, Func<Container, string, Type, object>> Lifetime()
         {
-            var l = new Singleton();
-            return l.LifetimeModifier;
+            return new Singleton().LifetimeModifier;
         }
     }
 }
