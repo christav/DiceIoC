75e8c3cd20c3a34c1cab13306d570aa694137bdb
Chris Tavares <ctavares@microsoft.com>
2013-12-13 22:27:04 -0800

    Minor cleanup

--- Performance/CastleWindsorUseCase.cs
+++ Performance/CastleWindsorUseCase.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.Windsor;
+﻿using Castle.Windsor;
 using Castle.MicroKernel.Registration;
 
 using Domain;
