95b389215b8f0257ccdb55c2ba9d820b7b401dce
Chris Tavares <ctavares@microsoft.com>
2014-04-27 12:57:44 -0700

    Supporting lifetimes that don't implement Enter

--- DiceIoC/Lifetimes/Lifetime.cs
+++ DiceIoC/Lifetimes/Lifetime.cs
@@ -9,8 +9,8 @@ namespace DiceIoC.Lifetimes
     /// is any object that implements the following methods:
     /// 
     /// IDisposable Enter();
-    /// object GetValue(Container c);
-    /// object SetValue(object value, Container c);
+    /// object GetValue(IContainer c);
+    /// object SetValue(object value, IContainer c);
     /// 
     /// No need for an interface or base class, just implement
     /// the methods as named above publicly and it's set.
@@ -20,36 +20,63 @@ namespace DiceIoC.Lifetimes
         /// <summary>
         /// Helper function for building lifetimes. This method will
         /// rewrite the given <paramref name="factoryExpression"/>
-        /// so that it is wrapped in calls to the <paramref name="lifetimeContainer"/>
+        /// so that it is wrapped in calls to the <paramref name="lifetimeManager"/>
         /// methods correctly.
         /// </summary>
         /// <remarks>The lifetime object doesn't have to derive from Lifetime,
         /// or even implement ILifetime. It just needs to implement the
         /// three methods Enter, GetValue, SetValue.</remarks>
-        /// <typeparam name="T">Type of the <paramref name="lifetimeContainer"/> to use.</typeparam>
+        /// <typeparam name="T">Type of the <paramref name="lifetimeManager"/> to use.</typeparam>
         /// <param name="factoryExpression">Original factory expression</param>
-        /// <param name="lifetimeContainer">Lifetime container object that will be called by
+        /// <param name="lifetimeManager">Lifetime container object that will be called by
         /// the rewritten expression.</param>
         /// <returns>The new expression.</returns>
         public static Expression<Func<IContainer, object>> RewriteForLifetime<T>(
             Expression<Func<IContainer, object>> factoryExpression, 
-            T lifetimeContainer)
+            T lifetimeManager)
         {
+            Expression body;
             var c = Expression.Parameter(typeof(IContainer), "container");
-            var guard = Expression.Parameter(typeof(IDisposable), "guard");
-            var ltm = Expression.Constant(lifetimeContainer, typeof(T));
 
-            var body = Expression.Block(new[] { guard },
-                CreateGuard(guard, ltm),
-                Expression.TryFinally(
-                    GetSetValueExpression(factoryExpression, c, ltm),
-                    Expression.Call(guard, "Dispose", null)
-                    )
-                );
+            if (typeof (T).GetMethod("Enter") != null)
+            {
+                body = BodyUsingEnter(c, factoryExpression, lifetimeManager);
+            }
+            else
+            {
+                body = BodyWithoutEnter(c, factoryExpression, lifetimeManager);
+            }
 
             var final = Expression.Lambda<Func<IContainer, object>>(
                 body, c);
             return final;
+        }
+
+        private static Expression BodyUsingEnter<T>(
+            ParameterExpression container,
+            Expression<Func<IContainer, object>> factoryExpression,
+            T lifetimeManager)
+        {
+            var guard = Expression.Parameter(typeof(IDisposable), "guard");
+            var ltm = Expression.Constant(lifetimeManager, typeof(T));
+
+            var body = Expression.Block(new[] { guard },
+                CreateGuard(guard, ltm),
+                Expression.TryFinally(
+                    GetSetValueExpression(factoryExpression, container, ltm),
+                    Expression.Call(guard, "Dispose", null)
+                    )
+                );
+
+            return body;
+        }
+
+        private static Expression BodyWithoutEnter<T>(ParameterExpression container,
+            Expression<Func<IContainer, object>> factoryExpression,
+            T lifetimeManager)
+        {
+            var ltm = Expression.Constant(lifetimeManager, typeof (T));
+            return GetSetValueExpression(factoryExpression, container, ltm);
         }
 
         private static Expression CreateGuard(ParameterExpression guardVariable, ConstantExpression ltm)
